bb4057baa5eb9884dedb3006e0b2f677ec4aeefc
Shahzad Ali <108119561+shahzadali50@users.noreply.github.com>
2024-03-28 15:48:28 +0500

    react project

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='py-4' style={{  background:"gainsboro" }}>
-      <p className='text-center ' style={{ color:"gray", fontSize:"18px" }} >  © 2024 Shahzad  All Rights Reserved  | Terms & Conditions  </p>
+      <p className='text-center ' style={{ color:"gray", fontSize:"18px" }} >  © 2024 Shahzad  All Rights Reserved | Terms & Conditions  </p>
     </footer>
   )
 }
